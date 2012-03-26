5ea90d10769dd2b83102e4c2a93e520b84282c77
Craig Weber <crgwbr@gmail.com>
2012-03-26 08:46:33 -0400

    minified code

--- server/annotare/views.py
+++ server/annotare/views.py
@@ -2,7 +2,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from server import settings
 
-@login_required()
+@login_required
 def index(request):
-    return render_to_response('index.html', {}, context_instance=RequestContext(request))
+    return render_to_response('index.html', {'DEBUG': settings.DEBUG}, context_instance=RequestContext(request))
